d22007a8850717460233d9ee45daef1963e72d56
Andrei <129526090+CruzAndrei@users.noreply.github.com>
2025-11-12 21:03:15 +0800

    added base for retention
    testing of retention data

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/header";
 import LineChartComponent from "../components/LineChartComponent";
 import Slurry from "../database/Slurry";
+import Retention from "../database/Slurry";
 
 export default function DashboardPage() {
   return (
@@ -10,6 +11,7 @@ export default function DashboardPage() {
       <h2 style={{ color: "#fff", marginTop: 20 }}>Dashboard Page</h2>
     <LineChartComponent /> //placeholder
     <Slurry />
+    <Retention/>
 
     </div>
   );
